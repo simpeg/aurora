92c2b8597b6cd8515d8b3e6fa47667214f3cce7c
Karl N. Kappler <magnetotellurics@gmail.com>
2021-05-29 12:18:04 -0700

    factored each window type into its own separate test

--- tests/test_apodization_window.py
+++ tests/test_apodization_window.py
@@ -2,42 +2,68 @@ import numpy as np
 
 from aurora.signal.apodization_window import ApodizationWindow
 
-def test_can_inititalize_apodization_window():
-    """
-    """
+def test_default_boxcar():
     apodization_window = ApodizationWindow(num_samples_window=4)
     print(apodization_window.summary)
-    apodization_window = ApodizationWindow(taper_family='hamming', num_samples_window=128)
+
+def test_hamming():
+    apodization_window = ApodizationWindow(taper_family='hamming',
+                                           num_samples_window=128)
     print(apodization_window.summary)
-    apodization_window = ApodizationWindow(taper_family='blackmanharris', num_samples_window=256)
+
+def test_blackmanharris():
+    apodization_window = ApodizationWindow(taper_family='blackmanharris',
+                                           num_samples_window=256)
     print(apodization_window.summary)
-    apodization_window = ApodizationWindow(taper_family='kaiser', num_samples_window=128, additional_args={"beta":8})
+
+def test_kaiser():
+    apodization_window = ApodizationWindow(taper_family='kaiser',
+                                           num_samples_window=128,
+                                           additional_args={"beta": 8})
     print(apodization_window.summary)
+
+def test_dpss():
     #<?>
     # Passes locally but fails on miniconda ci test... not sure why,
     # maybe not yet standard part of scipy.signal?  Get a to-be-deprecated
     # warning when call slepian. dpss is supposed to replace it.
-    # apodization_window = ApodizationWindow(taper_family='dpss',
-    #                                        num_samples_window=64,
-    #                                        additional_args={"NW":3.0})
-    #print(apodization_window.summary)
+    apodization_window = ApodizationWindow(taper_family='dpss',
+                                           num_samples_window=64,
+                                           additional_args={"NW":3.0})
+    print(apodization_window.summary)
     # </?>
 
     #<?>
     #sorting out which of slepian or dpss is causing failure.
     #just muted dpss and got a fail.
-    
+
+
+def test_slepian():
     apodization_window = ApodizationWindow(taper_family='slepian',
                                            num_samples_window=64,
-                                           additional_args={"width":0.3})
+                                           additional_args={"width": 0.3})
     print(apodization_window.summary)
-    apodization_window = ApodizationWindow(taper_family='custom', num_samples_window=64,
+
+def test_custom():
+    apodization_window = ApodizationWindow(taper_family='custom',
+                                           num_samples_window=64,
                                            taper=np.abs(np.random.randn(64)))
     print(apodization_window.summary)
-    #</?>
+
+def test_can_inititalize_apodization_window():
+    """
+    """
+    pass
 
 def main():
-    test_can_inititalize_apodization_window()
+    test_default_boxcar()
+    test_hamming()
+    test_blackmanharris()
+    test_kaiser()
+    test_slepian()
+    test_dpss()
+    test_custom()
+#    test_can_inititalize_apodization_window()
 
 if __name__ == "__main__":
     main()
