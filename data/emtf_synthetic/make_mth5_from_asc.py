aedf80955900ca9982ccb23fe46e0601fd4657a2
Karl N. Kappler <magnetotellurics@gmail.com>
2021-06-27 14:53:22 -0700

    process synthetic data works on phase, rho needs calibration

--- data/emtf_synthetic/make_mth5_from_asc.py
+++ data/emtf_synthetic/make_mth5_from_asc.py
@@ -8,23 +8,28 @@ from pathlib import Path
 import pandas as pd
 from mth5.timeseries import ChannelTS, RunTS
 from mth5.mth5 import MTH5
+from mt_metadata.timeseries.filters.coefficient_filter import CoefficientFilter
 
+#<GLOBAL CFG>
 INPUT_ASCII_DATA = Path(r"test1.asc")
 SYNTHETIC_DATA = INPUT_ASCII_DATA.parent.joinpath("emtf_synthetic.h5")
-#fn = Path(r"c:\Users\jpeacock\Documents\GitHub\aurora\data\emtf_synthetic
-# \test1.asc")
+RUN_ID = "001"
+STATION_ID = "mt001"
+SAMPLE_RATE = 1.0
+CF = CoefficientFilter()
+#</GLOBAL CFG>
+
 
 def create_mth5_synthetic_file(plot=False):
     df = pd.read_csv(INPUT_ASCII_DATA,
                      names=["hx", "hy", "hz", "ex", "ey"], sep="\s+")
 
-    sample_rate = 1.0
     # loop over stations and make them ChannelTS objects
     ch_list = []
     for col in df.columns:
         data = df[col].values
         meta_dict = {"component": col,
-                     "sample_rate": sample_rate}
+                     "sample_rate": SAMPLE_RATE}
         if col in ["ex", "ey"]:
             chts = ChannelTS(channel_type="electric", data=data,
                              channel_metadata=meta_dict)
@@ -41,8 +46,8 @@ def create_mth5_synthetic_file(plot=False):
     runts = RunTS(array_list=ch_list)
 
     # add in metadata
-    runts.station_metadata.id = "mt001"
-    runts.run_metadata.id = "001"
+    runts.station_metadata.id = STATION_ID
+    runts.run_metadata.id = RUN_ID
 
     # plot the data
     if plot:
@@ -51,17 +56,124 @@ def create_mth5_synthetic_file(plot=False):
     # make an MTH5
     m = MTH5()
     m.open_mth5(SYNTHETIC_DATA, mode="w")
-    station_group = m.add_station("mt001")
-    run_group = station_group.add_run("001")
+    station_group = m.add_station(STATION_ID)
+    run_group = station_group.add_run(RUN_ID)
     run_group.from_runts(runts)
 
     m.close_mth5()
 
 
 def read_the_sythetic_mth5():
+    import matplotlib.pyplot as plt
+    from aurora.general_helper_functions import SANDBOX
+    from aurora.time_series.windowing_scheme import WindowingScheme
+    from aurora.time_series.frequency_band import FrequencyBands
+    from aurora.time_series.frequency_band_helpers import extract_band
+    from aurora.transfer_function.transfer_function_header import \
+        TransferFunctionHeader
+    from aurora.transfer_function.TTFZ import TTFZ
+    from aurora.transfer_function.TRME import TRME
+    from aurora.transfer_function.TRegression import RegressionEstimator
+
+    #<CONFIG>
+    BAND_SETUP = "EMTF"
+    BAND_SETUP_FILE = SANDBOX.joinpath("bs_256.cfg")
+    NUM_SAMPLES_WINDOW = 256
+    TF_LOCAL_SITE = "PKD      "  # This comes from mth5/mt_metadata aurora#18
+    TF_REMOTE_SITE = None  # "SAO"   #This comes from mth5/mt_metadata aurora#18
+    TF_PROCESSING_SCHEME = "OLS"  # ""RME" #"OLS","RME", #required
+    TF_INPUT_CHANNELS = ["hx", "hy"]  # optional, default ["hx", "hy"]
+    TF_OUTPUT_CHANNELS = ["ex", "ey"]  # optional, default ["ex", "ey", "hz"]
+    TF_REFERENCE_CHANNELS = None  # optional, default ["hx", "hy"],
+    #<CONFIG>
     m = MTH5()
-    m.open_mth5(SYNTHETIC_DATA, mode="r")
+    m.open_mth5(SYNTHETIC_DATA, mode="a")
+    #m.survey_group.
+    run_obj = m.get_run(STATION_ID, RUN_ID)
+    runts = run_obj.to_runts()
+    ds = runts.dataset
+    windowing_scheme = WindowingScheme(taper_family="hamming",
+                                        num_samples_window=NUM_SAMPLES_WINDOW,
+                                        num_samples_overlap=192,
+                                        sampling_rate=runts.sample_rate)
+    windowed_obj = windowing_scheme.apply_sliding_window(runts.dataset)
+    print("windowed_obj", windowed_obj)
+    tapered_obj = windowing_scheme.apply_taper(windowed_obj)
+    print("tapered_obj", tapered_obj)
+    stft_obj = windowing_scheme.apply_fft(tapered_obj)
+    print("stft_obj", stft_obj)
+    #<CALIBRATE>
+    # for channel_id in stft_obj.keys():
+    #     mth5_channel = run_obj.get_channel(channel_id)
+    #     channel_filter = mth5_channel.channel_response_filter
+    #     calibration_response = channel_filter.complex_response(
+    #     stft_obj.frequency.data)
+    #
+    # if UNITS == "SI":
+    #     if channel_id[0].lower() == 'h':
+    #         calibration_response /= 1e-9  # SI Units
+    # stft_obj[channel_id].data /= calibration_response
+    #</CALIBRATE>
+
+    stft_obj_xrda = stft_obj.to_array("channel")
+#     frequencies = stft_obj.frequency.data[1:]
+#     # print(f"Lower Bound:{frequencies[0]}, Upper bound:{frequencies[-1]}")
+#
+    frequency_bands = FrequencyBands()
+    if BAND_SETUP == "EMTF":
+        frequency_bands.from_emtf_band_setup(filepath=BAND_SETUP_FILE,
+            sampling_rate = SAMPLE_RATE,
+            decimation_level = 1,
+            num_samples_window = NUM_SAMPLES_WINDOW)
+    elif BAND_SETUP == "XXXX":
+        print("TODO:Write a method to choose lower and upper bounds, "
+              "and number of bands to split it into")
+
+    transfer_function_header = TransferFunctionHeader(
+                    processing_scheme = TF_PROCESSING_SCHEME,
+                    local_site = TF_LOCAL_SITE,
+                    remote_site = TF_REMOTE_SITE,
+                    input_channels = TF_INPUT_CHANNELS,
+                    output_channels = TF_OUTPUT_CHANNELS,
+                    reference_channels = TF_REFERENCE_CHANNELS)
+    transfer_function_obj = TTFZ(transfer_function_header,
+    frequency_bands.number_of_bands)
+
+    for i_band in range(frequency_bands.number_of_bands):
+        band = frequency_bands.band(i_band)
+        band_dataarray = extract_band(band, stft_obj_xrda)
+        band_dataset = band_dataarray.to_dataset("channel")
+        X = band_dataset[TF_INPUT_CHANNELS]
+        Y = band_dataset[TF_OUTPUT_CHANNELS]
+        if TF_PROCESSING_SCHEME == "OLS":
+            regression_estimator = RegressionEstimator(X=X, Y=Y)
+            Z = regression_estimator.estimate_ols()
+            print(f"{TF_PROCESSING_SCHEME}, \n {Z}")
+        elif TF_PROCESSING_SCHEME=="RME":
+            iter_control = IterControl(max_number_of_iterations=MAX_NUMBER_OF_ITERATIONS)
+            regression_estimator = TRME(X=X, Y=Y, iter_control=iter_control)
+            Z = regression_estimator.estimate()
+            print(f"RME{TF_PROCESSING_SCHEME}, \n {Z}")
+        else:
+            print(f"processing_scheme {TF_PROCESSING_SCHEME} not supported")
+            print(f"processing_scheme must be one of OLS, RME "
+            f"not supported")
+            raise Exception
+
+        transfer_function_obj.set_tf(i_band, regression_estimator, band.center_period)
+
+    transfer_function_obj.apparent_resistivity()
+    from aurora.transfer_function.rho_plot import RhoPlot
+
+    plotter = RhoPlot(transfer_function_obj)
+    fig, axs = plt.subplots(nrows=2)
+    plotter.rho_sub_plot(axs[0])
+    plotter.phase_sub_plot(axs[1])
+    # plotter.rho_plot2()
+    # plotter.phase_plot()
+    plt.show()
     print("OK")
+
 
 
 def main():
