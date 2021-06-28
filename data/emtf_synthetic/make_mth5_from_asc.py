af4b322b69b419eeede0f6dbe499cfc855598f53
Karl N. Kappler <magnetotellurics@gmail.com>
2021-06-28 16:17:05 -0700

    synthetic data test now can use filters

--- data/emtf_synthetic/make_mth5_from_asc.py
+++ data/emtf_synthetic/make_mth5_from_asc.py
@@ -4,6 +4,7 @@ Created on Fri Jun 25 16:03:21 2021
 
 @author: jpeacock
 """
+import numpy as np
 from pathlib import Path
 import pandas as pd
 from mth5.timeseries import ChannelTS, RunTS
@@ -19,18 +20,42 @@ SAMPLE_RATE = 1.0
 COEFF_FILTER = CoefficientFilter()
 #</GLOBAL CFG>
 
+def make_coefficient_filter(gain=1.0, name="unit_conversion"):
+    #in general, you need to add all required fields from the
+    #standards.json
+    coeff_filter = CoefficientFilter()
+    cf = CoefficientFilter()
+    cf.units_in = "digital counts"
+    cf.units_out = "millivolts"
+    cf.gain = gain
+    cf.name = name
+    return cf
 
+# make filters:
+ACTIVE_FILTERS = []
+cf1 = make_coefficient_filter(name="1")
+cf10 = make_coefficient_filter(gain=100, name="10")
+ACTIVE_FILTERS = [cf1, cf10]
+COLUMNS = ["hx", "hy", "hz", "ex", "ey"]
 def create_mth5_synthetic_file(plot=False):
     df = pd.read_csv(INPUT_ASCII_DATA,
                      names=["hx", "hy", "hz", "ex", "ey"], sep="\s+")
+    for col in COLUMNS:
+        df[col] += 0.0*np.random.randn(len(df))
 
     # loop over stations and make them ChannelTS objects
+    # we need to add a tag in the channels
+    # so that when you call a run it will get all the filters with it.
     ch_list = []
     for col in df.columns:
         data = df[col].values
-        meta_dict = {"component": col,
-                     "sample_rate": SAMPLE_RATE}
+        # meta_dict = {"component": col,
+        #              "sample_rate": SAMPLE_RATE,
+        #              "filter.name":[cf1.name, cf10.name]}
         if col in ["ex", "ey"]:
+            meta_dict = {"component": col,
+                         "sample_rate": SAMPLE_RATE,
+                         "filter.name": [cf1.name,]}
             chts = ChannelTS(channel_type="electric", data=data,
                              channel_metadata=meta_dict)
             # add metadata to the channel here
@@ -38,6 +63,9 @@ def create_mth5_synthetic_file(plot=False):
 
 
         elif col in ["hx", "hy", "hz"]:
+            meta_dict = {"component": col,
+                         "sample_rate": SAMPLE_RATE,
+                         "filter.name": [cf1.name, cf10.name]}
             chts = ChannelTS(channel_type="magnetic", data=data,
                              channel_metadata=meta_dict)
 
@@ -54,7 +82,6 @@ def create_mth5_synthetic_file(plot=False):
     if plot:
         runts.plot()
 
-    # add the filter:
     #survey = Survey()
     #survey.filters = {'filter1':COEFF_FILTER}
 
@@ -64,6 +91,9 @@ def create_mth5_synthetic_file(plot=False):
     station_group = m.add_station(STATION_ID)
     run_group = station_group.add_run(RUN_ID)
     run_group.from_runts(runts)
+    #add filters
+    for fltr in ACTIVE_FILTERS:
+        cf_group = m.filters_group.add_filter(fltr)
 
     m.close_mth5()
 
@@ -74,6 +104,7 @@ def read_the_sythetic_mth5():
     from aurora.time_series.windowing_scheme import WindowingScheme
     from aurora.time_series.frequency_band import FrequencyBands
     from aurora.time_series.frequency_band_helpers import extract_band
+    from aurora.transfer_function.iter_control import IterControl
     from aurora.transfer_function.transfer_function_header import \
         TransferFunctionHeader
     from aurora.transfer_function.TTFZ import TTFZ
@@ -84,12 +115,14 @@ def read_the_sythetic_mth5():
     BAND_SETUP = "EMTF"
     BAND_SETUP_FILE = SANDBOX.joinpath("bs_256.cfg")
     NUM_SAMPLES_WINDOW = 256
+    MAX_NUMBER_OF_ITERATIONS = 10
     TF_LOCAL_SITE = "PKD      "  # This comes from mth5/mt_metadata aurora#18
     TF_REMOTE_SITE = None  # "SAO"   #This comes from mth5/mt_metadata aurora#18
-    TF_PROCESSING_SCHEME = "OLS"  # ""RME" #"OLS","RME", #required
+    TF_PROCESSING_SCHEME = "RME"  # ""RME" #"OLS","RME", #required
     TF_INPUT_CHANNELS = ["hx", "hy"]  # optional, default ["hx", "hy"]
     TF_OUTPUT_CHANNELS = ["ex", "ey"]  # optional, default ["ex", "ey", "hz"]
     TF_REFERENCE_CHANNELS = None  # optional, default ["hx", "hy"],
+    UNITS = "SI"
     #<CONFIG>
     m = MTH5()
     m.open_mth5(SYNTHETIC_DATA, mode="a")
@@ -108,16 +141,16 @@ def read_the_sythetic_mth5():
     stft_obj = windowing_scheme.apply_fft(tapered_obj)
     print("stft_obj", stft_obj)
     #<CALIBRATE>
-    # for channel_id in stft_obj.keys():
-    #     mth5_channel = run_obj.get_channel(channel_id)
-    #     channel_filter = mth5_channel.channel_response_filter
-    #     calibration_response = channel_filter.complex_response(
-    #     stft_obj.frequency.data)
-    #
+    for channel_id in stft_obj.keys():
+        mth5_channel = run_obj.get_channel(channel_id)
+        channel_filter = mth5_channel.channel_response_filter
+        calibration_response = channel_filter.complex_response(
+        stft_obj.frequency.data)
+
     # if UNITS == "SI":
     #     if channel_id[0].lower() == 'h':
     #         calibration_response /= 1e-9  # SI Units
-    # stft_obj[channel_id].data /= calibration_response
+        stft_obj[channel_id].data /= calibration_response
     #</CALIBRATE>
 
     stft_obj_xrda = stft_obj.to_array("channel")
@@ -177,6 +210,9 @@ def read_the_sythetic_mth5():
     # plotter.rho_plot2()
     # plotter.phase_plot()
     plt.show()
+    print(transfer_function_obj.rho.shape)
+    print(transfer_function_obj.rho[0])
+    print(transfer_function_obj.rho[-1])
     print("OK")
 
 
