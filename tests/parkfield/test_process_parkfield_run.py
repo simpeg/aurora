3f616007517b28b2d9371ff487a60fad77601e77
Karl N. Kappler <magnetotellurics@gmail.com>
2021-08-28 13:46:21 -0700

    added make_mth5 call to test_process_parkfield_run in case data are not found

--- tests/parkfield/test_process_parkfield_run.py
+++ tests/parkfield/test_process_parkfield_run.py
@@ -4,7 +4,14 @@ from aurora.general_helper_functions import TEST_PATH
 
 processing_cfg = Path(TEST_PATH, "parkfield", "config", "pkd_run_config.json")
 mth5_path = Path(TEST_PATH, "parkfield", "data", "pkd_test_00.h5")
+
 run_id = "001"
+
+if not mth5_path.exists():
+    from make_parkfield_mth5 import test_make_parkfield_mth5
+
+    test_make_parkfield_mth5()
+
 tf_collection = process_mth5_run(
     processing_cfg, run_id, mth5_path=mth5_path, units="SI"
 )
