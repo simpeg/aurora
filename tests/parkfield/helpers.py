4f8276b33b68516b56e1f7fe40bdb90976e66128
Karl N. Kappler <magnetotellurics@gmail.com>
2021-09-07 15:24:40 -0700

    add file

---
+++ tests/parkfield/helpers.py
@@ -0,0 +1,5 @@
+from aurora.general_helper_functions import TEST_PATH
+
+PARKFIELD_PATH = TEST_PATH.joinpath("parkfield")
+DATA_PATH = PARKFIELD_PATH.joinpath("data")
+CONFIG_PATH = PARKFIELD_PATH.joinpath("config")
