7f88aa013843312f1042b00991d5051f24aacb3b
Karl N. Kappler <magnetotellurics@gmail.com>
2021-09-09 10:43:08 -0700

    added dataset description for SAO RR test data

--- aurora/sandbox/io_helpers/make_dataset_configs.py
+++ aurora/sandbox/io_helpers/make_dataset_configs.py
@@ -21,7 +21,7 @@ def make_test_configs():
     # test_data_set.channel_codes = "LQ2,LQ3,LT1,LT2"
     test_data_set.channel_codes = "BQ2,BQ3,BT1,BT2"
     test_data_set.description = "2h of PKD data for 2004-09-28 midnight UTC until 0200"
-    test_data_set.components_list = HEXY
+    test_data_set.components_list = ["ex", "ey", "hx", "hy"]
 
     test_data_set_configs[test_data_set.dataset_id] = test_data_set
     # </pkd_test_00 Single station>
@@ -39,7 +39,22 @@ def make_test_configs():
     test_data_set.channel_codes = "BQ2,BQ3,BT1,BT2"
     test_data_set.description = "2h of SAO data for 2004-09-28 midnight UTC until 0200"
     test_data_set.components_list = HEXY
+    test_data_set_configs[test_data_set.dataset_id] = test_data_set
+    # </sao_test_00 Single station>
 
+    # <pkd_sao_test_00 Remote Reference>
+    test_data_set = IRISDatasetConfig()
+    test_data_set.dataset_id = "pkd_sao_test_00"
+    test_data_set.network = "BK"
+    test_data_set.station = "PKD,SAO"
+    test_data_set.starttime = UTCDateTime("2004-09-28T00:00:00.000000Z")
+    test_data_set.endtime = UTCDateTime("2004-09-28T01:59:59.975000Z")
+    # test_data_set.endtime = UTCDateTime("2004-09-28T00:01:59.999000Z") #small test
+    test_data_set.channel_codes = "BQ2,BQ3,BT1,BT2"
+    test_data_set.description = (
+        "2h of PKD,SAO data for 2004-09-28 midnight UTC until 0200"
+    )
+    test_data_set.components_list = HEXY
     test_data_set_configs[test_data_set.dataset_id] = test_data_set
     # </sao_test_00 Single station>
 
