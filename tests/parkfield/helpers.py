000a37405f0c58f16b6a0088470f8aed17e99e04
Karl N. Kappler <magnetotellurics@gmail.com>
2021-09-14 16:11:13 -0700

    reorder, add comment

--- tests/parkfield/helpers.py
+++ tests/parkfield/helpers.py
@@ -1,7 +1,13 @@
 from aurora.general_helper_functions import TEST_PATH
 
+
 PARKFIELD_PATH = TEST_PATH.joinpath("parkfield")
-DATA_PATH = PARKFIELD_PATH.joinpath("data")
-CONFIG_PATH = PARKFIELD_PATH.joinpath("config")
-EMTF_RESULTS_PATH = PARKFIELD_PATH.joinpath("emtf_results")
+
 AURORA_RESULTS_PATH = PARKFIELD_PATH.joinpath("aurora_results")
+CONFIG_PATH = PARKFIELD_PATH.joinpath("config")
+DATA_PATH = PARKFIELD_PATH.joinpath("data")
+EMTF_RESULTS_PATH = PARKFIELD_PATH.joinpath("emtf_results")
+
+# May want to create results and data dir on init
+# AURORA_RESULTS_PATH.mkdir(exist_ok=True)
+# DATA_PATH.mkdir(exist_ok=True)
