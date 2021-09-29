8022c44d1fa1b5624e09ecd1698cc226a363d20e
Karl N. Kappler <magnetotellurics@gmail.com>
2021-09-29 15:45:28 -0700

    add issue #112 folder to tests/

---
+++ tests/issue112/a/test_a.py
@@ -0,0 +1,13 @@
+from helpers import TMP
+
+
+def test_it():
+    assert TMP == "112a"
+
+
+def main():
+    test_it()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/issue112/b/test_b.py
@@ -0,0 +1,13 @@
+from helpers import TMP
+
+
+def test_it():
+    assert TMP == "112b"
+
+
+def main():
+    test_it()
+
+
+if __name__ == "__main__":
+    main()
