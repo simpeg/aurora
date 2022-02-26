098e94ddd2a0de2c73b82b4648c8bece704c41d9
JP <peacock.jared@gmail.com>
2022-02-26 14:30:59 -0800

    added band

--- aurora/config/metadata/band.py
+++ aurora/config/metadata/band.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 17 14:15:20 2022
+Created on Fri Feb 25 15:20:59 2022
 
 @author: jpeacock
 """
@@ -11,31 +11,16 @@ from mt_metadata.base.helpers import write_lines
 from mt_metadata.base import get_schema, Base
 from .standards import SCHEMA_FN_PATHS
 
-from .window import Window
-from .decimation import Decimation
-
+# =============================================================================
+attr_dict = get_schema("band", SCHEMA_FN_PATHS)
 
 # =============================================================================
-attr_dict = get_schema("decimation_level", SCHEMA_FN_PATHS)
-attr_dict.add_dict(get_schema("decimation"), "decimation")
-attr_dict.add_dict(get_schema("window"), "window")
-attr_dict.add_dict(get_schema("regression"), "regression")
-
-# =============================================================================
-class DecimationLevel(Base):
+class Band(Base):
     __doc__ = write_lines(attr_dict)
 
     def __init__(self, **kwargs):
         
-        self.window = Window()
-        self.decimation = Decimation()
-        
-        self.bands = []
-        
         super().__init__(attr_dict=attr_dict, **kwargs)
+     
+    # should add properties to calculate index from frequency and vise-versa
 
-        
-
-        
-    
-    
