eb4b5c7dd4884e54d5233ecae5ba9ac2c6c471ec
Karl N. Kappler <magnetotellurics@gmail.com>
2022-05-13 16:13:45 -0700

    added test for channel_scale_factors

--- tests/synthetic/test_synthetic_driver.py
+++ tests/synthetic/test_synthetic_driver.py
@@ -3,6 +3,7 @@ from aurora.test_utils.synthetic.make_mth5_from_asc import create_test1_h5
 from aurora.test_utils.synthetic.make_mth5_from_asc import create_test2_h5
 from aurora.test_utils.synthetic.make_mth5_from_asc import create_test12rr_h5
 from aurora.test_utils.synthetic.make_processing_configs import create_test_run_config
+from aurora.test_utils.synthetic.paths import AURORA_RESULTS_PATH
 from aurora.test_utils.synthetic.paths import CONFIG_PATH
 from aurora.test_utils.synthetic.processing_helpers import process_sythetic_data
 from aurora.tf_kernel.dataset import DatasetDefinition
@@ -39,7 +40,7 @@ from aurora.tf_kernel.helpers import extract_run_summaries_from_mth5s
 #     process_mth5_run(test_config, run_id, units="MT")
 
 
-def process_synthetic_1():
+def process_synthetic_1(z_file_path="", test_scale_factor=False):
     """
 
     Returns
@@ -54,9 +55,27 @@ def process_synthetic_1():
     dataset_definition = DatasetDefinition()
     dataset_definition.df = dataset_df
     #Test that channel_scale_factors column is optional
-    dataset_df.drop(columns=["channel_scale_factors"], inplace=True)
+    if test_scale_factor:
+        scale_factors = {'ex': 10.0, 'ey': 3.0, 'hx': 6.0, 'hy': 5.0, 'hz': 100.0}
+        dataset_df["channel_scale_factors"].at[0] = scale_factors
+    else:
+        dataset_df.drop(columns=["channel_scale_factors"], inplace=True)
     processing_config = create_test_run_config("test1", dataset_df)
-    tfc = process_sythetic_data(processing_config, dataset_definition)
+    tfc = process_sythetic_data(processing_config,
+                                dataset_definition,
+                                z_file_path=z_file_path)
+
+    z_figure_name = z_file_path.name.replace("zss", "png")
+    for xy_or_yx in ["xy", "yx"]:
+        ttl_str = f"{xy_or_yx} component, test_scale_factor = {test_scale_factor}"
+        out_png_name = f"{xy_or_yx}_{z_figure_name}"
+        tfc.rho_phi_plot(
+            xy_or_yx=xy_or_yx,
+            ttl_str=ttl_str,
+            show=False,
+            figure_basename=out_png_name,
+            figure_path=AURORA_RESULTS_PATH
+        )
     return tfc
 
 
@@ -88,13 +107,27 @@ def test_process_mth5():
     """
     Runs several synthetic processing tests from config creation to tf_collection.
     TODO: Modify these tests so that they output tfs using mt_metadata transfer function
+
+    2022-05-13: Added a duplicate run of process_synthetic_1, which is intended to
+    test the channel_scale_factors in the new mt_metadata processing class.  Expected
+    outputs are four .png:
+    xy_syn1.png : Shows expected 100 Ohm-m resisitivity
+    xy_syn1_scaled.png : Overestimates by 4x for 300 Ohm-m resistivity
+    yx_syn1.png : Shows expected 100 Ohm-m resisitivity
+    yx_syn1_scaled.png : Underestimates by 4x for 25 Ohm-m resistivity
+    These .png are stores in aurora_results folder
+
     Returns
     -------
 
     """
     # process_synthetic_1_underdetermined()
     # process_synthetic_1_with_nans()
-    tfc = process_synthetic_1()
+
+    z_file_path=AURORA_RESULTS_PATH.joinpath("syn1.zss")
+    tfc = process_synthetic_1(z_file_path=z_file_path)
+    z_file_path=AURORA_RESULTS_PATH.joinpath("syn1_scaled.zss")
+    tfc = process_synthetic_1(z_file_path=z_file_path, test_scale_factor=True)
     tfc = process_synthetic_2()
     tfc = process_synthetic_rr12()
 
