308be7225ae1f378a99b165737e2ca2dfb490f14
Karl N. Kappler <magnetotellurics@gmail.com>
2022-05-28 14:49:15 -0700

    Factor run summary creator
    Modified to allow extract_run_summaries_from_mth5s to accept mth5
    objects as well as (already supported) types of pathlib.Path or str

--- aurora/tf_kernel/helpers.py
+++ aurora/tf_kernel/helpers.py
@@ -2,13 +2,28 @@ import pandas as pd
 
 from aurora.tf_kernel.dataset import DatasetDefinition
 
+from mth5.mth5 import MTH5
 from mth5.utils.helpers import initialize_mth5
 
-def extract_run_summaries_from_mth5s(mth5_paths, type="run", deduplicate=True):
+def extract_run_summary_from_mth5(mth5_obj, type="run"):
+    ch_summary = mth5_obj.channel_summary
+    if type == "run":
+        dataset_definition = DatasetDefinition()
+        dataset_definition.from_mth5_channel_summary(ch_summary)
+        df = dataset_definition.df
+        df["mth5_path"] = str(mth5_obj.filename)
+    elif type == "channel":
+        df = ch_summary.to_dataframe()
+    return df
+
+def extract_run_summaries_from_mth5s(mth5_list, type="run", deduplicate=True):
     """
     ToDo: Move this method into mth5? or mth5_helpers?
     ToDo: Make this a class so that the __repr__ is a nice visual representation of the
     df, like what channel summary does in mth5
+
+    2022-05-28 Modified to allow this method to accept mth5 objects as well as the
+    already supported types of pathlib.Path or str
 
     Given a list of mth5s, this returns a dataframe of all available runs
 
@@ -29,21 +44,22 @@ def extract_run_summaries_from_mth5s(mth5_paths, type="run", deduplicate=True):
     super_summary: pd.DataFrame
 
     """
-    dfs = len(mth5_paths) * [None]
+    dfs = len(mth5s) * [None]
 
-    for i, mth5_path in enumerate(mth5_paths):
-        mth5_obj = initialize_mth5(mth5_path, mode="a")
-        ch_summary = mth5_obj.channel_summary
-        if type == "run":
-            dataset_definition = DatasetDefinition()
-            dataset_definition.from_mth5_channel_summary(ch_summary)
-            df = dataset_definition.df
-            df["mth5_path"] = str(mth5_path)
-        elif type == "channel":
-            df = ch_summary.to_dataframe()
-        mth5_obj.close_mth5()
+    for i, mth5_elt in enumerate(mth5_list):
+        if isinstance(mth5_elt, MTH5):
+            mth5_obj = mth5_elt
+        else:   #its a path or a string
+            mth5_obj = initialize_mth5(mth5_elt, mode="a")
+
+        df = extract_run_summary_from_mth5(mth5_obj, type=type)
+
+        #close it back up if you opened it
+        if not isinstance(mth5_elt, MTH5):
+            mth5_obj.close_mth5()
         dfs[i] = df
 
+    #merge all summaries into a super_summary
     super_summary = pd.concat(dfs)
     super_summary.reset_index(drop=True, inplace=True)
     if deduplicate:
