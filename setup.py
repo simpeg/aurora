bdd74771ae9c94bcbb85adbfff7574d3c613bbfb
Karl N. Kappler <magnetotellurics@gmail.com>
2022-07-09 13:15:20 -0700

    Bump Version, implemented KernelDataset

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/simpeg/aurora",
-    version="0.0.3",
+    version="0.0.4",
     zip_safe=False,
 )
