69098f80bae8cf9868c60ccb1ce2b1e264e36afa
Karl N. Kappler <magnetotellurics@gmail.com>
2022-07-23 13:34:19 -0700

    restore placeholder (#194)

---
+++ examples/placeholder.py
@@ -0,0 +1,30 @@
+"""
+Placeholder for example
+===========================
+
+This example is a placeholder that uses the sphinx-gallery syntax
+for creating examples
+"""
+
+import aurora
+import numpy as np
+import matplotlib.pyplot as plt
+
+###############################################################################
+# Step 1
+# ------
+# Some description of what we are doing in step one
+
+x = np.linspace(0, 4 * np.pi, 100)
+
+# take the sin(x)
+y = np.sin(x)
+
+
+###############################################################################
+# Step 2
+# ------
+# Plot it
+
+fig, ax = plt.subplots(1, 1)
+ax.plot(x, y)
