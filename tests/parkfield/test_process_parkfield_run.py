b4de48e26c7661c474a09fd80f0c89d6ddbcc8a7
Karl N. Kappler <magnetotellurics@gmail.com>
2022-08-20 13:36:15 -0700

    Simplify processing config initialization
    While working on doc and notebooks for workshop, I realize that the
    processing config always needs its stations assigned explictly by the
    user before calling the pipeline,  but this information is actually
    already in the kernel datset, so this explicit assignment can be
    avoided, to simplify use.
    
    To that end I sligthly modified the start of the pipeline so that
    if the config doesn't have a local station, it gets the station info
    from the kernel dataset.
    
    Also updated ipynb accordingly.

--- tests/parkfield/test_process_parkfield_run.py
+++ tests/parkfield/test_process_parkfield_run.py
@@ -50,7 +50,6 @@ def test_processing(return_collection=False, z_file_path=None, test_clock_zero=F
     config = cc.create_run_processing_object(
         emtf_band_file=BANDS_DEFAULT_FILE, sample_rate=40.0, estimator={"engine": "RME"}
     )
-    config.stations.from_dataset_dataframe(tfk_dataset.df)
 
     if test_clock_zero:
         for dec_lvl_cfg in config.decimations:
