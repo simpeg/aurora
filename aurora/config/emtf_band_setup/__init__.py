dc2f270238cc74225be35bce2d8a1873e5f1e00b
Karl N. Kappler <magnetotellurics@gmail.com>
2022-09-09 08:09:29 -0700

    Add __init__ to import bands default files

---
+++ aurora/config/emtf_band_setup/__init__.py
@@ -0,0 +1,6 @@
+from aurora.general_helper_functions import BAND_SETUP_PATH
+import pathlib
+
+BANDS_DEFAULT_FILE = BAND_SETUP_PATH.joinpath("bs_test.cfg")
+BANDS_256_26_FILE = BAND_SETUP_PATH.joinpath("bs_256_26.cfg")
+BANDS_256_29_FILE = BAND_SETUP_PATH.joinpath("bs_256_29.cfg")
