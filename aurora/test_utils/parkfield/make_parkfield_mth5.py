a1cf95d14d7680d6581da0cc32c946e4525cf0a0
Karl N. Kappler <magnetotellurics@gmail.com>
2022-12-31 16:39:24 -0800

    update data source to https

--- aurora/test_utils/parkfield/make_parkfield_mth5.py
+++ aurora/test_utils/parkfield/make_parkfield_mth5.py
@@ -13,59 +13,18 @@ from aurora.sandbox.io_helpers.make_mth5_helpers import (
 )
 from aurora.test_utils.parkfield.path_helpers import DATA_PATH
 
-# create_from_server DEPRECATED 2021-09-18
-# def create_from_server(dataset_config, data_source="IRIS", target_folder=Path()):
-#
-#     # <GET EXPERIMENT>
-#     inventory = dataset_config.get_inventory_from_client(
-#         base_url=data_source, ensure_inventory_stages_are_named=True
-#     )
-#     translator = XMLInventoryMTExperiment()
-#     experiment = translator.xml_to_mt(inventory_object=inventory)
-#     # </GET EXPERIMENT>
-#
-#     # <TRIAGE ONE-OFF ISSUE WITH UNITS>
-#     experiment = triage_mt_units_electric_field(experiment)
-#     # </TRIAGE ONE-OFF ISSUE WITH UNITS>
-#
-#     # <INITIALIZE MTH5>
-#     h5_path = target_folder.joinpath(dataset_config.h5_filebase)
-#     mth5_obj = initialize_mth5(h5_path)
-#     mth5_obj.from_experiment(experiment)
-#     # </INITIALIZE MTH5>
-#     dataset_config.describe()
-#
-#     # <GET DATA STREAMS>
-#     streams = dataset_config.get_data_via_fdsn_client(data_source=data_source)
-#     streams = make_channel_labels_fdsn_compliant(streams)
-#     run_stream = trim_streams_to_acquisition_run(streams)
-#     # </GET DATA STREAMS>
-#
-#     # <THIS NEEDS TO BE WRAPPED IN MULTI STATION /MULTI RUN LOGIC>
-#     station_group = mth5_obj.get_station(dataset_config.station)
-#     run_metadata = experiment.surveys[0].stations[0].runs[0]
-#     run_ts_obj = RunTS()
-#     run_ts_obj.from_obspy_stream(run_stream, run_metadata)
-#     run_id = "001"
-#     run_ts_obj.run_metadata.id = run_id
-#     run_group = station_group.add_run(run_id)
-#     run_group.from_runts(run_ts_obj)
-#     # </THIS NEEDS TO BE WRAPPED IN MULTI STATION /MULTI RUN LOGIC>
-#     mth5_obj.close_mth5()
-#
-#     return
+PKDSAO_DATA_SOURCE = "https://service.ncedc.org/"  # "NCEDC"
 
 
 def make_parkfield_mth5():
-    # close_open_files()
+    close_open_files()
     makeparkfield_h5_path = DATA_PATH.joinpath("pkd_test_00.h5")
-    print(f" 0 makeparkfield_h5_path.exists() {makeparkfield_h5_path.exists()}")
     dataset_id = "pkd_test_00"
     print(f" 1 makeparkfield_h5_path.exists() {makeparkfield_h5_path.exists()}")
     dataset_config = TEST_DATA_SET_CONFIGS[dataset_id]
     create_from_server_multistation(
         dataset_config,
-        data_source="NCEDC",
+        data_source=PKDSAO_DATA_SOURCE,
         target_folder=DATA_PATH,
         triage_units="V/m to mV/km",
     )
@@ -83,7 +42,7 @@ def make_parkfield_hollister_mth5():
     dataset_config = TEST_DATA_SET_CONFIGS[dataset_id]
     create_from_server_multistation(
         dataset_config,
-        data_source="NCEDC",
+        data_source=PKDSAO_DATA_SOURCE,
         target_folder=DATA_PATH,
         triage_units="V/m to mV/km",
     )
@@ -101,16 +60,6 @@ def make_parkfield_hollister_mth5():
 #     h5_path = DATA_PATH.joinpath(f"{dataset_id}.h5")
 #     run, runts = read_back_data(h5_path, "SAO", "001")
 #     print("hello")
-#
-#
-
-# <TEST FAILS BECAUSE DATA NOT AVAILABLE FROM IRIS -- NEED TO REQUEST FROM NCEDC>
-# def test_make_hollister_mth5():
-#     dataset_id = "sao_test_00"
-#     create_from_iris(dataset_id)
-#     h5_path = DATA_PATH.joinpath(f"{dataset_id}.h5")
-#     read_back_data(h5_path, "SAO", "001")
-# </TEST FAILS BECAUSE DATA NOT AVAILABLE FROM IRIS -- NEED TO REQUEST FROM NCEDC>
 
 
 def main():
