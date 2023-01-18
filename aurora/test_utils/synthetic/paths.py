0ba99345d9d941368be1b73927cf47d7f3ed7f0b
Karl N. Kappler <magnetotellurics@gmail.com>
2023-01-17 20:17:02 -0800

    force synthetic config directory to be created

--- aurora/test_utils/synthetic/paths.py
+++ aurora/test_utils/synthetic/paths.py
@@ -5,4 +5,6 @@ CONFIG_PATH = SYNTHETIC_PATH.joinpath("config")
 DATA_PATH = SYNTHETIC_PATH.joinpath("data")
 EMTF_OUTPUT_PATH = SYNTHETIC_PATH.joinpath("emtf_output")
 AURORA_RESULTS_PATH = SYNTHETIC_PATH.joinpath("aurora_results")
+
 AURORA_RESULTS_PATH.mkdir(exist_ok=True)
+CONFIG_PATH.mkdir(exist_ok=True)
