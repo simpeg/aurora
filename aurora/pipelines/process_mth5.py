6dd10742674e5043c887ffb92750a5c581b1a4d7
Karl N. Kappler <magnetotellurics@gmail.com>
2023-02-11 12:19:51 -0800

    docstrings, and shorten check of valid run

--- aurora/pipelines/process_mth5.py
+++ aurora/pipelines/process_mth5.py
@@ -5,17 +5,18 @@ Note 1: process_mth5 assumes application of cascading decimation, and that the
 decimated data will be accessed from the previous decimation level.  This should be
 revisited. It may make more sense to have a get_decimation_level() interface that
 provides an option of applying decimation or loading predecimated data.
+This will be addressed via creation of the FC layer inside mth5.
 
-#Note 2: Question: Can we run into cases where some of these runs can be decimated
-and others can not?  We need a way to handle this. For example, a short run may not
-yield any data from a later decimation level. Returning an empty xarray may work.
-It is desireable that the empty xarray, or whatever comes back to pass through STFT
-and MERGE steps smoothy. If it is empty, we could just drop the run entirely but
-this may have adverse consequences on downstream bookkeeping, like the creation of
-station_metadata before packaging the tf for export. This could be worked around by
-extracting the metadata at the start of this method. It would be a good idea in
-general to run a pre-check on the data that identifies which decimation levels are
-valid for each run. (see Issue #182)
+Note 2: We can encounter cases where some runs can be decimated and others can not.
+We need a way to handle this. For example, a short run may not yield any data from a
+later decimation level. An attempt to handle this has been made in TF Kernel by
+adding a is_valid_dataset column, associated with each run-decimation level pair.
+
+
+Note 3: This point in the loop marks the interface between _generation_ of the FCs and
+ their _usage_. In future the code above this comment would be pushed into
+ create_fourier_coefficients() and the code below this would access those FCs and
+ execute compute_transfer_function()
 
 """
 # =============================================================================
@@ -226,8 +227,7 @@ def update_dataset_df(i_dec_level, config, tfk=None):
         # See Note 1 top of module
         # See Note 2 top of module
         for i, row in tfk.dataset_df.iterrows():
-            valid = tfk.is_valid_dataset(row, i_dec_level)
-            if not valid:
+            if not tfk.is_valid_dataset(row, i_dec_level):
                 continue
             run_xrds = row["run_dataarray"].to_dataset("channel")
             decimated_run_xrds = prototype_decimate(config.decimation, run_xrds)
@@ -288,9 +288,6 @@ def process_mth5(
     # ANY MERGING OF RUNS IN TIME DOMAIN WOULD GO HERE
     tfk.dataset.initialize_dataframe_for_processing(mth5_objs)
 
-    # Place checks that would be done by TF Kernel here
-    # see notes labelled with ToDo TFK above
-
     print(
         f"Processing config indicates {len(tfk.config.decimations)} "
         f"decimation levels "
@@ -310,9 +307,8 @@ def process_mth5(
         local_stfts = []
         remote_stfts = []
         for i, row in tfk.dataset_df.iterrows():
-            # Check that this row is valid
-            valid = tfk.is_valid_dataset(row, i_dec_level)
-            if not valid:
+
+            if not tfk.is_valid_dataset(row, i_dec_level):
                 continue
 
             run_xrds = row["run_dataarray"].to_dataset("channel")
@@ -334,7 +330,9 @@ def process_mth5(
         else:
             remote_merged_stft_obj = None
 
-        # Could mute bad FCs here - Not implemented yet.
+        # FC TF Interface here (see Note #3)
+
+        # Could downweight bad FCs here
 
         tf_obj = process_tf_decimation_level(
             tfk.config,
