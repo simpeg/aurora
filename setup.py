db4bdc6d05049090b41e35f74c016d7cd966dae2
Karl N. Kappler <magnetotellurics@gmail.com>
2023-04-22 11:00:26 -0700

    update version to match main

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/simpeg/aurora",
-    version="0.0.5",
+    version="0.2.0",
     zip_safe=False,
 )
