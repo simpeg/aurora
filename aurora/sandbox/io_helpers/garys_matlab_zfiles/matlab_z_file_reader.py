c9b66e1970b51a02a8d196ee74a02f2824bb89e3
Karl N. Kappler <magnetotellurics@gmail.com>
2023-09-18 13:08:31 -0700

    tidy and add some notes about flow

--- aurora/sandbox/io_helpers/garys_matlab_zfiles/matlab_z_file_reader.py
+++ aurora/sandbox/io_helpers/garys_matlab_zfiles/matlab_z_file_reader.py
@@ -8,19 +8,26 @@ import numpy as np
 import pandas as pd
 import scipy.io as sio
 
-
 from aurora.config.metadata.processing import Processing
 from aurora.config.emtf_band_setup import BANDS_256_29_FILE
 from aurora.general_helper_functions import TEST_PATH
+from aurora.pipelines.transfer_function_kernel import TransferFunctionKernel
 from aurora.sandbox.io_helpers.emtf_band_setup import EMTFBandSetupFile
-from aurora.sandbox.io_helpers.zfile_murphy import read_z_file
 from aurora.transfer_function.emtf_z_file_helpers import clip_bands_from_z_file
-from aurora.transfer_function.plot.rho_phi_helpers import plot_phi
-from aurora.transfer_function.plot.rho_phi_helpers import plot_rho
+from aurora.transfer_function.kernel_dataset import KernelDataset
 from aurora.transfer_function.transfer_function_collection import (
     TransferFunctionCollection,
 )
+from mt_metadata.timeseries.survey import Survey
+from mt_metadata.transfer_functions.core import TF
 
+def read_matlab_z_file(case_id, z_mat):
+    tmp = sio.loadmat(z_mat)
+    if case_id == "synthetic":
+        tmp = tmp["temp"][0][0].tolist()
+    elif case_id == "IAK34ss":
+        tmp = tmp["TFstruct"][0][0].tolist()
+    return tmp
 
 def test_matlab_zfile_reader(case_id="IAK34ss", make_plot=False):
     """
@@ -47,8 +54,13 @@ def test_matlab_zfile_reader(case_id="IAK34ss", make_plot=False):
 
     """
     test_dir_path = TEST_PATH.joinpath("io")
-    bs_file = BANDS_256_29_FILE
+
+    # 1. Define processing parameters based on which of the two tests are to be run
     if case_id == "synthetic":
+        band_setup_file = BANDS_256_29_FILE
+        field_data_sample_rate = 1.0
+        num_samples_window = 256
+        decimation_factors = [1, 4, 4, 4]
         n_periods_clip = 3  # for synthetic case
         estimator_engine = "RME"
         local_station_id = "test1"
@@ -60,10 +72,14 @@ def test_matlab_zfile_reader(case_id="IAK34ss", make_plot=False):
             "ey",
         ]
         reference_channels = []
-        z_mat = "TS1zss20210831.mat"
+        matlab_z_file = "TS1zss20210831.mat"
         archived_z_file_path = None
         z_file_path = "from_matlab.zss"
     elif case_id == "IAK34ss":
+        band_setup_file = BANDS_256_29_FILE
+        field_data_sample_rate = 1.0
+        num_samples_window = 256
+        decimation_factors = [1, 4, 4, 4]
         n_periods_clip = 3
         estimator_engine = "RME"
         local_station_id = "IAK34"
@@ -75,25 +91,25 @@ def test_matlab_zfile_reader(case_id="IAK34ss", make_plot=False):
             "ey",
         ]
         reference_channels = []
-        z_mat = test_dir_path.joinpath("IAK34_struct_zss.mat")
+        matlab_z_file = test_dir_path.joinpath("IAK34_struct_zss.mat")
         archived_z_file_path = test_dir_path.joinpath("archived_from_matlab.zss")
         z_file_path = test_dir_path.joinpath("from_matlab.zss")
 
-    sample_rate = 1.0
-    tf_dict = {}
-
+    # 2. Create an aurora processing config
     p = Processing()
     p.stations.local.id = local_station_id
     if remote_station_id:
         p.stations.remote.id = remote_station_id
-    emtf_band_setup = EMTFBandSetupFile(filepath=bs_file, sample_rate=sample_rate)
-    num_samples_window = 256
-    decimation_factors = [1, 4, 4, 4]
+    emtf_band_setup = EMTFBandSetupFile(filepath=band_setup_file, sample_rate=field_data_sample_rate)
     band_edges = emtf_band_setup.compute_band_edges(
         decimation_factors=decimation_factors,
         num_samples_window=4 * [num_samples_window],
     )
-    p.assign_bands(band_edges, sample_rate, decimation_factors, num_samples_window)
+    p.assign_bands(band_edges, field_data_sample_rate, decimation_factors, num_samples_window)
+
+    # 3. populate decimation levels of processing config
+    tf_dict = {}
+    sample_rate = field_data_sample_rate
     for i_dec in range(4):
         p.decimations[i_dec].decimation.sample_rate = sample_rate
         p.decimations[i_dec].window.num_samples = num_samples_window
@@ -106,20 +122,18 @@ def test_matlab_zfile_reader(case_id="IAK34ss", make_plot=False):
         tf_dict[i_dec] = tffz_obj
         sample_rate /= 4.0
 
-    tmp = sio.loadmat(z_mat)
-    if case_id == "synthetic":
-        stuff = tmp["temp"][0][0].tolist()
-    elif case_id == "IAK34ss":
-        stuff = tmp["TFstruct"][0][0].tolist()
-    matlab_tf = stuff[4]
-    periods = stuff[5]
-    cov_ss = stuff[7]
-    cov_nn = stuff[8]
-    n_data = stuff[9]
-    R2 = stuff[10]
-    # stderr = stuff[12]
-    # freq = stuff[14]
+    # 4. Read in the matlab_z object
+    tmp = read_matlab_z_file(case_id, matlab_z_file)
+    matlab_tf = tmp[4]
+    periods = tmp[5]
+    cov_ss = tmp[7]
+    cov_nn = tmp[8]
+    n_data = tmp[9]
+    R2 = tmp[10]
+    # stderr = tmp[12]
+    # freq = tmp[14]
 
+    # 4a. FIX NAN / INF
     nan_cov_nn = []
     for i in range(len(periods)):
         if np.isnan(cov_nn[:, :, i]).any():
@@ -132,8 +146,7 @@ def test_matlab_zfile_reader(case_id="IAK34ss", make_plot=False):
         for i in range(12):
             cov_nn[:, :, i] = cov_nn[:, :, 12]
 
-    # FIX NAN / INF
-
+    # 5. Pack the TF dicts with data values
     tf_dict[0].tf.data = matlab_tf[:, :, :11]
     tf_dict[0].cov_nn.data = cov_nn[:, :, :11]
     tf_dict[0].cov_ss_inv.data = cov_ss[:, :, :11]
@@ -158,13 +171,9 @@ def test_matlab_zfile_reader(case_id="IAK34ss", make_plot=False):
     tf_dict[3].num_segments.data = n_data[:, 23:]
     tf_dict[3].R2.data = R2[:, 23:]
 
+    # 6. Create TF Collection, and TFKernel
     tfc = TransferFunctionCollection(tf_dict=tf_dict, processing_config=p)
-    from aurora.pipelines.transfer_function_kernel import TransferFunctionKernel
-    from aurora.transfer_function.kernel_dataset import KernelDataset
-    from mt_metadata.timeseries.survey import Survey
-    from mt_metadata.transfer_functions.core import TF
     kd = KernelDataset(local_station_id=local_station_id)
-
     survey_metadata = Survey()
     kd.survey_metadata["0"] = survey_metadata
     kd_df_dict = {'remote': [False, ],
@@ -174,17 +183,20 @@ def test_matlab_zfile_reader(case_id="IAK34ss", make_plot=False):
     kd_df = pd.DataFrame(data=kd_df_dict)
     kd.df = kd_df
     tfk = TransferFunctionKernel(dataset=kd, config=p)
-    #pd.DataFrame({"survey":"0", "station":local_station_id})
+
+    # 7. Create mt_metadata TF object and write z-file
     tf_obj = tfk.export_tf_collection(tfc)
     tf_obj.station_metadata.id = local_station_id
     tf_obj.station_metadata.runs[0].channels["ey"].measurement_azimuth = 90.0
     tf_obj.station_metadata.runs[0].channels["hy"].measurement_azimuth = 90.0
     tf_obj.write(z_file_path)
 
+    # 7a. clip last few periods (matlab object and z-objects provided were not identical)
     if n_periods_clip:
         clip_bands_from_z_file(z_file_path, n_periods_clip, n_sensors=5)
 
 
+    # 8. Compare z-file written by TF object to the archived version
     tf_obj_from_z_file = TF()
     tf_obj_from_z_file.from_zmm(z_file_path)
     archived_tf = TF()
