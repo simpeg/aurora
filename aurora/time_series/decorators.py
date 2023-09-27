395e1d1d0bce4102d68d5858f88391a69a877195
Laura Keyson <laura@iris.washington.edu>
2023-09-27 15:55:06 -0700

    escaping * in docs

--- aurora/time_series/decorators.py
+++ aurora/time_series/decorators.py
@@ -2,9 +2,9 @@
 # Here is the decorator pattern
 def decorator(func):
     @functools.wraps(func)
-    def wrapper_decorator(*args, **kwargs):
+    def wrapper_decorator(\*args, \*\*kwargs):
         # Do something before
-        value = func(*args, **kwargs)
+        value = func\*args, \*\*kwargs)
         # Do something after
         return value
 
