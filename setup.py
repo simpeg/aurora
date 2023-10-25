a89a4b7a9f00c6282db2fb55f627440c922819bd
kkappler <magnetotellurics@gmail.com>
2023-10-25 06:36:37 -0700

    bumpversion patch
    - Main change here is support for mth5 files that predate current version
    - workarounds in place for these "antique" files, including run_id in channel summary
    differing from that in mth5 hieriarchy (e.g. '1' vs '001')
    - also added support for read only data on gadi

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/simpeg/aurora",
-    version="0.3.3",
+    version="0.3.4",
     zip_safe=False,
 )
