f37386867e89472f6f28c85d445e0ea6c830ec75
Karl N. Kappler <magnetotellurics@gmail.com>
2023-11-11 08:08:36 -0800

    bumpversion patch, ready for testing on gadi

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/simpeg/aurora",
-    version="0.3.10",
+    version="0.3.11",
     zip_safe=False,
 )
