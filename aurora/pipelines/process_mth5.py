aa824dca3c460362300eeea70b053b1b7d09b2e3
Karl N. Kappler <magnetotellurics@gmail.com>
2024-01-26 17:24:04 -0800

    bug fix

--- aurora/pipelines/process_mth5.py
+++ aurora/pipelines/process_mth5.py
@@ -108,7 +108,7 @@ def station_obj_from_row(row):
     """
     if row.mth5_obj.file_version == "0.1.0":
         station_obj = row.mth5_obj.stations_group.get_station(row.station_id)
-    elif row.mth5_obj.file_version == "0.1.0":
+    elif row.mth5_obj.file_version == "0.2.0":
         survey_group = row.mth5_obj.surveys_group.get_survey(row.survey)
         station_obj = survey_group.stations_group.get_station(row.station_id)
     return station_obj
