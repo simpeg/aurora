d40e58bf3dfdd28eaf12a51047cf8caad3154baf
Karl N. Kappler <magnetotellurics@gmail.com>
2024-03-16 18:14:07 -0700

    bumpversion

--- aurora/__init__.py
+++ aurora/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.12"
+__version__ = "0.3.13"
 
 import sys
 from loguru import logger
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/simpeg/aurora",
-    version="0.3.12",
+    version="0.3.13",
     zip_safe=False,
 )
