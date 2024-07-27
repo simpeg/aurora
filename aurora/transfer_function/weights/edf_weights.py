1814a7b624793f282337c7a07a8a717c687e634e
Karl N. Kappler <magnetotellurics@gmail.com>
2024-07-26 17:22:58 -0700

    add doc, #337

--- aurora/transfer_function/weights/edf_weights.py
+++ aurora/transfer_function/weights/edf_weights.py
@@ -1,26 +1,39 @@
 """
-from egbert_codes-20210121T193218Z-001/egbert_codes/matlabPrototype_10-13-20/TF
-/functions/Edfwts.m
+    This module contains a class for computing so-called "Effective Degrees of Freedom" weights.
+
+Development notes:
+The code here is based on the function Edfwts.m from egbert_codes-
+20210121T193218Z-001/egbert_codes/matlabPrototype_10-13-20/TF/functions/Edfwts.m
+
 """
 
 import numpy as np
+import xarray as xr
 from loguru import logger
-
+from typing import Optional, Union
 import aurora.transfer_function.weights.edf_weights
 
 
 class EffectiveDegreesOfFreedom(object):
-    def __init__(self, **kwargs):
+    def __init__(
+        self,
+        edf_l1: Optional[float] = 20.0,
+        alpha: Optional[float] = 0.5,
+        c1: Optional[float] = 2.0,
+        c2: Optional[float] = 10.0,
+        p3: Optional[float] = 5.0,
+        n_data: Optional[int] = 0,
+    ):
         """
+        Constructor.
 
         Parameters
         ----------
-        kwargs
-        n_data : integer
+        n_data : int
             Number of "numeric" observations in the input and remote reference data.
             Any time-frequency index having nan value in any channel is subtracted
             from the total number of observations before computation of p1, p2
-        edfl1 : int or float
+        edf_l1 : float
              the effective number of degrees of freedom at which a datapoint should be
               removed from the total energy calculation in the iterative loop
         alpha : float
@@ -30,19 +43,19 @@ class EffectiveDegreesOfFreedom(object):
         c2 : float
             ad hoc parameter for scaling the p2 threshold
         p3 : float
-            parameter controling the threshold at which the ratio of a data weighting
-            associated with remote and input channel is unaccepatble.
+            parameter controlling threshold at which the ratio of a data weighting
+            associated with remote and input channel is unacceptable.
         """
 
-        self.edfl1 = kwargs.get("edf_l1", 20.0)
-        self.alpha = kwargs.get("alpha", 0.5)
-        self.c1 = kwargs.get("c1", 2.0)
-        self.c2 = kwargs.get("c2", 10.0)
-        self.p3 = kwargs.get("p3", 5)
-        self.n_data = kwargs.get("n_data", 0)
+        self.edf_l1 = edf_l1
+        self.alpha = alpha
+        self.c1 = c1
+        self.c2 = c2
+        self.p3 = p3
+        self.n_data = n_data
 
     @property
-    def p1(self):
+    def p1(self) -> float:
         """
         Threshold applied to edf.  All edf below  this value
         are set to weight=0
@@ -50,64 +63,74 @@ class EffectiveDegreesOfFreedom(object):
         return self.c1 * (self.n_data**self.alpha)
 
     @property
-    def p2(self):
+    def p2(self) -> float:
         """
         Threshold applied to edf.  All edf above th  this value
         are set to weight=0
         """
         return self.c2 * (self.n_data**self.alpha)
 
-    def compute_weights(self, X, use):
+    def compute_weights(self, X: np.ndarray, use: np.ndarray) -> np.ndarray:
         """
+        Compute the EDF Weights
+
+        Development Notes:
         The data covariance matrix s and its inverse h are iteratively recomputed using
         fewer and fewer observations. However, the edf is also computed at every
         iteration but doesn't seem to use any fewer observations.
-        Thus the edf weights change as use drops, EVEN FOR INDICES that were
-        previously computed... this seems like it could be an error.
+        Thus the edf weights change as use drops, _even for indices_ that were
+        previously computed ... TODO: Could that be an error?
 
         Discussing this with Gary:
-        "... becuase you are downweighting (omitting) more and more highpower events
+        "... because you are down-weighting (omitting) more and more highpower events
         the total signal is going down.  The signal power goes down with every call
         to this method"
-
-        Parameters
-        ----------
-        X
-        use : boolean array
-
-        Returns
-        -------
-        The "HAT" Matrix, where the diagonals of this matrix are really big it means an
+        ...
+        "The "HAT" Matrix, where the diagonals of this matrix are really big it means an
         individual data point is controlling its own prediction, and the estimate.
         If the problem was balanced, each data point would contribute equally to the
-        estimate.  Each data point shoudl contribute 1/N to each parameter. When one
+        estimate.  Each data point should contribute 1/N to each parameter. When one
         data point is large and the others are tiny, then it may be contributing a
         lot, say 1/2 rather than 1/n.
-
         edf is like the diagonal of the Hat matrix (in the single station case)
         How much does the data point contribute to the prediction of itself.
         If there are n data points contributing equally, each datapoint
-        #should contribute ~1/n to its prediction
-
-        Note: H = inv(S) in general has equal H[0,1] = H[1,0];  2x2 matrices with
+        should contribute ~1/n to its prediction
+        - Note: H = inv(S) in general has equal H[0,1] = H[1,0];  2x2 matrices with
         matching off-diagonal terms have inverses with the same property.
+
+        Parameters
+        ----------
+        X: np.ndarray
+            The data to for which to determine weights.
+        use : np.ndarray
+            popolated with booleans
+
+        Returns
+        -------
+        edf:np.ndarray
+            The weights values.
 
         """
         S = X[:, use] @ np.conj(X[:, use]).T  # covariance matrix, 2x2
-        S /= sum(use)
-        H = np.linalg.inv(S)  # invert the 2x2
+        S /= sum(use)  # normalize by the number of datapoints
+        H = np.linalg.inv(S)  # inverse covariance matrix
 
         xx_term = np.real(X[0, :] * np.conj(X[0, :]) * H[0, 0])
         yy_term = np.real(X[1, :] * np.conj(X[1, :]) * H[1, 1])
-        xy_term = 2 * np.real(np.conj(X[1, :]) * X[0, :] * H[1, 0])
-        # ?real or abs?
+        xy_term = 2 * np.real(np.conj(X[1, :]) * X[0, :] * H[1, 0])  # real or abs?
         edf = xx_term + yy_term + xy_term
         return edf
 
 
-def effective_degrees_of_freedom_weights(X, R, edf_obj=None, test=True):
+def effective_degrees_of_freedom_weights(
+    X: xr.Dataset,
+    R: Union[xr.Dataset, None],
+    edf_obj=None,
+) -> np.ndarray:
     """
-    Emulates edfwts ("effective dof") from tranmt.  Based on Edfwts.m matlab code from
+    Computes the effective degrees of freedom weights. Emulates edfwts ("effective dof") from tranmt.
+    - Based on Edfwts.m matlab code from
     iris_mt_scratch/egbert_codes-20210121T193218Z-001/egbert_codes/matlabPrototype_10-13-20/TF/functions/
 
     Flow:
@@ -115,10 +138,18 @@ def effective_degrees_of_freedom_weights(X, R, edf_obj=None, test=True):
     1. Remove any nan in X,R
     2. compute the weights on the reduced (no-nan) arrays of X and R
     3. Overwrite the weights vector for the non-nan entries.
-    4 Return the weights vector.  We then broadcast multiply that array against the
-    data, to apply the weights.
+    4. Return weights and broadcast-multiply against data to apply.
 
-    TODO: This follows the matlab code by using a boolean index vector.  An xarray
+    Development Notes:
+    Note about the while loop: variable "use" never changes length, it only
+    flips its bit.  The while loop exits when n_valid_observations == sum(use)
+    i.e.the effective dof are all below threshold Estimate dof.  Then we "use"
+    only points whose dof are smaller than the threshold.  Then we recompute dof.
+    This time the covariance matrix diagonals are smaller, there is less energy
+    in the time series for the S, H calculation.
+
+    TODO:
+     This follows the matlab code by using a boolean index vector.  An xarray
      implementation which uses the observation dimension of X, R was started but never
      finished.  Here are the breadcrumbs for the xarray method:
      0. Create weights:
@@ -130,19 +161,24 @@ def effective_degrees_of_freedom_weights(X, R, edf_obj=None, test=True):
      from aurora.time_series.xarray_helpers import handle_nan
      X, Y, RR = handle_nan(XX, None, R, drop_dim="observation")
      2. Same as below
-     3. Overwrite: Not sure, it wasn't clear how to assign them. Possibly the syntax
-     with multi-index was confusing me when trying to assign.
+     3. Overwrite: Got stuck here.
      Accessing the weights was done by:
      weights.sel(observation=X.observation)
-     But the assignment syntax I'm not sure about.  Could also be that I need to cast
-     as dataarray for the assignment step.
+     Assignment should be something like weights.loc[x.observation]
+     May need cast as dataarray for the assignment step.
 
-    TODO: Why is the Remote reference weighting not done with a while loop?  Maybe
+    TODO:
+     Why is the Remote reference weighting not done with a while loop?  Maybe
      an oversight in the matlab codes.
-    TODO: Since zero-weights assigned to Nan, could probably remove the keep_indices
+
+    TODO:
+     Since zero-weights assigned to Nan, could probably remove the keep_indices
      methods and simply assign nan or zero weights to those data up front. Since the
      "use" boolean selects data before computation are performed on X, R we should
      never get nans in the computed edfs, but should simplify the code somewhat.
+
+    TODO:
+     Add tests for data having Nan values
 
     Parameters
     ----------
@@ -156,7 +192,7 @@ def effective_degrees_of_freedom_weights(X, R, edf_obj=None, test=True):
 
     Returns
     -------
-    weights : numpy array
+    weights : numpy.ndarray
         Weights for reducing leverage points.
 
     """
@@ -168,19 +204,6 @@ def effective_degrees_of_freedom_weights(X, R, edf_obj=None, test=True):
     X = X.to_array(dim="channel")
     if R is not None:
         R = R.to_array(dim="channel")
-
-    if test:
-        pass
-        # add_nan - this syntax only works for dataarray
-        # X[0,10] *= 2e1
-        # X[0,1] *= np.nan
-        # # X[0,-3] *= np.nan
-        # # try:
-        # #     X[0,-13] *= 1e13#np.nan
-        # # except IndexError:
-        # #     pass
-        # # if R is not None:
-        # #     R[1,2:11] *= np.nan
 
     n_observations_initial = len(X.observation)
     weights = np.ones(n_observations_initial)
@@ -199,21 +222,16 @@ def effective_degrees_of_freedom_weights(X, R, edf_obj=None, test=True):
     if edf_obj is None:
         edf_obj = EffectiveDegreesOfFreedom(n_data=n_observations_numeric)
 
-    # %    determine intial robust B-field cross-power matrix; this just uses
-    # %    edfl1 -- cut off for estimating robust local magnetic covariance
+    # %    determine initial robust B-field cross-power matrix; this just uses
+    # %    edf_l1 -- cut off for estimating robust local magnetic covariance
     nOmit = n_observations_numeric
     use = np.ones(n_observations_numeric, dtype=bool)
     # initialize use as a boolean of True, as large as XX
     n_valid_observations = n_observations_numeric
-    # "use" never changes length, it only flips its bit
-    # The while loop exits when n_valid_observations == sum(use)
-    # i.e.the effective dof are all below threshold
-    # Estimate dof.  Then we "use" only points whose dof are smaller than the
-    # threshold.  Then we recompute dof.  This time the covariance matrix diagonals
-    # are smaller, there is less energy in the time series for the S, H calculation.
+
     while nOmit > 0:
         eff_deg_of_freedom = edf_obj.compute_weights(XX, use)
-        use = eff_deg_of_freedom <= edf_obj.edfl1  # update "use" boolean selector
+        use = eff_deg_of_freedom <= edf_obj.edf_l1  # update "use" boolean selector
         nOmit = n_valid_observations - sum(use)
         n_valid_observations = sum(use)
 
