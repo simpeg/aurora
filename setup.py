ba58ee41a9cb15c86c0421c67a28bd4cac9847c0
Karl N. Kappler <magnetotellurics@gmail.com>
2024-08-28 17:49:26 -0700

    bumping patch version for pre-release

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/simpeg/aurora",
-    version="0.3.13",
+    version="0.3.14",
     zip_safe=False,
 )
