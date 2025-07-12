c46a2f37012a81123c1c8f7f984d69dd7ce4eb5a
Karl N. Kappler <magnetotellurics@gmail.com>
2025-07-11 17:01:07 -0700

    bumpversion

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/simpeg/aurora",
-    version="0.4.2",
+    version="0.5.0",
     zip_safe=False,
 )
