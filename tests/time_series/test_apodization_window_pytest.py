e0b27a4d23ee2fda3a0d0caf2aba6d6cfd90fd38
Karl N. Kappler <magnetotellurics@gmail.com>
2026-02-08 13:09:48 -0800

    raw string for comment to fix escape sequence warning

--- tests/time_series/test_apodization_window_pytest.py
+++ tests/time_series/test_apodization_window_pytest.py
@@ -216,22 +216,19 @@ class TestApodizationWindowCalculations:
                 assert np.isfinite(energy)
 
     def test_linear_spectral_density_factor(self, subtests) -> None:
-        """
+        r"""
         This is just a test to verify some algebra
 
         Claim:
         The lsd_calibration factors
-        A      (1./coherent\_gain)\*np.sqrt((2\*dt)/(nenbw\*N))
+        A      (1./coherent_gain)*np.sqrt((2*dt)/(nenbw*N))
         and
-        B      np.sqrt(2/(sample\_rate\*self.S2))
+        B      np.sqrt(2/(sample_rate*self.S2))
 
+        Note sqrt(2*dt)==sqrt(2*sample_rate) so we can cancel these terms and A=B IFF
 
-        Note sqrt(2\*dt)==sqrt(2\*sample_rate) so we can cancel these terms and
-        A=B IFF
-
-        (1./coherent_gain) \* np.sqrt(1/(nenbw\*N)) == 1/np.sqrt(S2)
-        which I show in githib aurora issue #3 via .
-        (CG\*\*2) \* NENBW \*N   =  S2
+        (1./coherent_gain) * np.sqrt(1/(nenbw*N)) == 1/np.sqrt(S2)
+        which is shown in github aurora issue #3 via (CG**2) * NENBW *N = S2
 
         """
         taper_families = ["boxcar", "hamming", "hann", "blackman"]
